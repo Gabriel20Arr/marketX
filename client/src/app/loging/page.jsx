9a898f32c7b07e2c5dac474da203c534f65cb9dc
Daniel David Serrano Gonzalez <daniel3112933987@gmail.com>
2023-06-13 21:44:11 -0500

    Avance primer día

--- client/src/app/loging/page.jsx
+++ client/src/app/loging/page.jsx
@@ -1,17 +1,14 @@
 'use client'
 
 import { useState } from "react"
-import Style from './registrar.module.css';
+import Style from './Loging.module.css';
 import image from '../../images/WhatsApp Image 2023-06-13 at 8.46.20 PM.jpeg';
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Registrarse() {
   const [usuario, setUsuario] = useState({
-    usuario:'',
     correo:'',
-    contraseña:'',
-    confirme_su_contraseña:''
+    contraseña:''
   });
 
   const handlerUsuario = (e)=>{
@@ -23,10 +20,8 @@ export default function Registrarse() {
     e.preventDefault();
     console.log(usuario);
     setUsuario({
-      usuario:'',
       correo:'',
-      contraseña:'',
-      confirme_su_contraseña:''
+      contraseña:''
     })
   }
 
@@ -38,17 +33,11 @@ export default function Registrarse() {
       </div>
       <div className={Style.form}>
         <h1>Registrese gratis</h1>
-        <span>usuario: </span>
-        <input type="text" name="usuario" onChange={handlerUsuario} value={usuario.usuario}/><br />
         <span>correo: </span>
         <input type="text" name="correo" onChange={handlerUsuario} value={usuario.correo}/><br />
         <span>contraseña: </span>
         <input type="text" name="contraseña" onChange={handlerUsuario} value={usuario.contraseña}/><br />
-        <span>confirme su contraseña: </span>
-        <input type="text" name="confirme_su_contraseña" onChange={handlerUsuario} value={usuario.confirme_su_contraseña}/><br />
-        
-          <button type="submit" onClick={handlerSubmit}><Link href='/loging'>Continuar </Link></button>
-       
+        <button type="submit" onClick={handlerSubmit}>Continuar</button>
       </div>
     </div>
   )
