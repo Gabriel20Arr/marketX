d0378408d617e9e815dbbea708ad214cfea1b6a7
Gabriel <gabriel2021arr@gmail.com>
2023-06-13 21:48:24 -0300

    rutas y handler de producto

--- app/src/routes/index.routes.js
+++ app/src/routes/index.routes.js
@@ -6,7 +6,7 @@ const ProductoRouter = require('./Producto.routes')
 router.use("/Producto", ProductoRouter)
 
 router.use("/Usuario", (req, res) => {
-    res.send(console.log("Prueba Usuario rutas"))
+    res.send(console.log("Prueba Usuario rutas"));
 })
 
 module.exports = router;
