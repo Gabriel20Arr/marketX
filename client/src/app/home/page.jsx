95423fbc9e37d47e4161b97437d789e87c10cd64
ostle <leoostrower@gmail.com>
2023-06-16 17:25:15 -0300

    ordenamiento funciona con db

--- client/src/app/home/page.jsx
+++ client/src/app/home/page.jsx
@@ -1,11 +1,37 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSortOrder } from '../../redux/features/sortSlice';
 import Paginacion from '../../components/Paginacion/Paginacion';
 
-export default async function HomePage() {
+export default function HomePage() {
+	const dispatch = useDispatch();
+	const [currentPage, setCurrentPage] = useState(0);
 
-  return (
-    <div>
-      <h1>ESTA ES LA HOME PAGE</h1>
-      <Paginacion />
-    </div>
-  )
+	const handleSortOrder = (order) => {
+		dispatch(setSortOrder(order));
+		setCurrentPage(0);
+	};
+
+	return (
+		<div>
+			<h1>ESTA ES LA HOME PAGE</h1>
+			<div>
+				<button onClick={() => handleSortOrder('title')}>A-Z</button>
+				<button onClick={() => handleSortOrder('reverse')}>Z-A</button>
+				<button onClick={() => handleSortOrder('price')}>
+					MENOR A MAYOR PRECIO
+				</button>
+				<button onClick={() => handleSortOrder('price-reverse')}>
+					MAYOR A MENOR PRECIO
+				</button>
+				<button onClick={() => handleSortOrder('quantitySold')}>
+					MÁS VENDIDO
+				</button>
+				<button onClick={() => handleSortOrder('reverse')}>RESTORE</button>
+			</div>
+			<Paginacion currentPage={currentPage} setCurrentPage={setCurrentPage} />
+		</div>
+	);
 }
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,18 +1,19 @@
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
-export default function Card({item}) {
-  const router = useRouter();
-  const handlerDetail = ()=>{
-    item.id?router.push(`/home/${item.id}`):console.log('no');
-  }
-  return (
-    <div>
-        {item.id?<article>
-            <h2 onClick={()=>handlerDetail()}>{item.titulo}</h2>
-            <h2>{item.precio}</h2>
-            <img src={item.imagen} alt="" />
-
-        </article>:null}
-    </div>
-  )
+export default function Card({ item }) {
+	const router = useRouter();
+	const handlerDetail = () => {
+		item.id ? router.push(`/home/${item.id}`) : console.log('no');
+	};
+	return (
+		<div>
+			{item.id ? (
+				<article>
+					<h2 onClick={() => handlerDetail()}>{item.titulo}</h2>
+					<h2>${item.precio}</h2>
+					<img src={item.imagen} alt='' />
+				</article>
+			) : null}
+		</div>
+	);
 }
