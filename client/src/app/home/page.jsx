8bbf51d103eba9cde3c7931d3880e9f6316ba4c6
ostle <leoostrower@gmail.com>
2023-06-16 18:21:26 -0300

    filtros y ordenamientos funcionan con db

--- client/src/app/home/page.jsx
+++ client/src/app/home/page.jsx
@@ -8,9 +8,19 @@ import Paginacion from '../../components/Paginacion/Paginacion';
 export default function HomePage() {
 	const dispatch = useDispatch();
 	const [currentPage, setCurrentPage] = useState(0);
+	const [selectedCategory, setSelectedCategory] = useState('');
 
 	const handleSortOrder = (order) => {
-		dispatch(setSortOrder(order));
+		if (order === 'restore') {
+			setSelectedCategory('');
+		} else {
+			dispatch(setSortOrder(order));
+		}
+		setCurrentPage(0);
+	};
+
+	const handleCategoryChange = (event) => {
+		setSelectedCategory(event.target.value);
 		setCurrentPage(0);
 	};
 
@@ -18,6 +28,12 @@ export default function HomePage() {
 		<div>
 			<h1>ESTA ES LA HOME PAGE</h1>
 			<div>
+				<select value={selectedCategory} onChange={handleCategoryChange}>
+					<option value=''>Todas las categorías</option>
+					<option value='Placas de video'>Placas de video</option>
+					<option value='Procesadores'>Procesadores</option>
+					<option value='Motherboards'>Motherboards</option>
+				</select>
 				<button onClick={() => handleSortOrder('title')}>A-Z</button>
 				<button onClick={() => handleSortOrder('reverse')}>Z-A</button>
 				<button onClick={() => handleSortOrder('price')}>
@@ -26,12 +42,16 @@ export default function HomePage() {
 				<button onClick={() => handleSortOrder('price-reverse')}>
 					MAYOR A MENOR PRECIO
 				</button>
-				<button onClick={() => handleSortOrder('quantitySold')}>
+				{/* <button onClick={() => handleSortOrder('quantitySold')}>
 					MÁS VENDIDO
-				</button>
-				<button onClick={() => handleSortOrder('reverse')}>RESTORE</button>
+				</button> */}
+				<button onClick={() => handleSortOrder('restore')}>RESTORE</button>
 			</div>
-			<Paginacion currentPage={currentPage} setCurrentPage={setCurrentPage} />
+			<Paginacion
+				currentPage={currentPage}
+				setCurrentPage={setCurrentPage}
+				selectedCategory={selectedCategory}
+			/>
 		</div>
 	);
 }
--- client/src/components/Paginacion/Paginacion.jsx
+++ client/src/components/Paginacion/Paginacion.jsx
@@ -11,34 +11,47 @@ import style from './Paginacion.module.css';
 import Cards from '../Cards/Cards';
 import { productos } from '../../api/api';
 
-const Paginacion = ({ currentPage, setCurrentPage }) => {
+const Paginacion = ({ currentPage, setCurrentPage, selectedCategory }) => {
 	const sortOrder = useSelector((state) => state.sort.sortOrder);
 
 	const sortProducts = (products) => {
+		let sortedProducts = [...products];
+
+		if (selectedCategory !== '') {
+			sortedProducts = sortedProducts.filter(
+				(product) => product.categoria === selectedCategory
+			);
+		}
+
 		if (sortOrder === 'title') {
-			return products.slice().sort((a, b) => a.titulo.localeCompare(b.titulo));
+			return sortedProducts
+				.slice()
+				.sort((a, b) => a.titulo.localeCompare(b.titulo));
 		} else if (sortOrder === 'price') {
-			return products
+			return sortedProducts
 				.slice()
 				.sort((a, b) => parseFloat(a.precio) - parseFloat(b.precio));
 		} else if (sortOrder === 'price-reverse') {
-			return products
+			return sortedProducts
 				.slice()
 				.sort((a, b) => parseFloat(b.precio) - parseFloat(a.precio));
 		} else if (sortOrder === 'quantitySold') {
-			return products
+			return sortedProducts
 				.slice()
 				.sort((a, b) => parseInt(a.cantidadVenta) - parseInt(b.cantidadVenta));
 		} else if (sortOrder === 'quantitySold-reverse') {
-			return products
+			return sortedProducts
 				.slice()
 				.sort((a, b) => parseInt(b.cantidadVenta) - parseInt(a.cantidadVenta));
 		} else if (sortOrder === 'reverse') {
-			return products.slice().reverse();
+			return sortedProducts.slice().reverse();
 		}
+
+		return sortedProducts;
 	};
 
-	const { data, error, isLoading, isFetching } = useGetProductsQuery(null);
+	const { data, error, isLoading, isFetching } =
+		useGetProductsQuery(selectedCategory);
 	const apiProductos = data || [];
 	const items = [...apiProductos, ...productos];
 
@@ -91,3 +104,97 @@ const Paginacion = ({ currentPage, setCurrentPage }) => {
 };
 
 export default Paginacion;
+
+// 'use client';
+
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import {
+// 	useGetProductsQuery,
+// 	useGetProductsByIdQuery,
+// } from '@/src/redux/services/productApi';
+// import ReactPaginate from 'react-paginate';
+// import style from './Paginacion.module.css';
+// import Cards from '../Cards/Cards';
+// import { productos } from '../../api/api';
+
+// const Paginacion = ({ currentPage, setCurrentPage }) => {
+// 	const sortOrder = useSelector((state) => state.sort.sortOrder);
+
+// 	const sortProducts = (products) => {
+// 		if (sortOrder === 'title') {
+// 			return products.slice().sort((a, b) => a.titulo.localeCompare(b.titulo));
+// 		} else if (sortOrder === 'price') {
+// 			return products
+// 				.slice()
+// 				.sort((a, b) => parseFloat(a.precio) - parseFloat(b.precio));
+// 		} else if (sortOrder === 'price-reverse') {
+// 			return products
+// 				.slice()
+// 				.sort((a, b) => parseFloat(b.precio) - parseFloat(a.precio));
+// 		} else if (sortOrder === 'quantitySold') {
+// 			return products
+// 				.slice()
+// 				.sort((a, b) => parseInt(a.cantidadVenta) - parseInt(b.cantidadVenta));
+// 		} else if (sortOrder === 'quantitySold-reverse') {
+// 			return products
+// 				.slice()
+// 				.sort((a, b) => parseInt(b.cantidadVenta) - parseInt(a.cantidadVenta));
+// 		} else if (sortOrder === 'reverse') {
+// 			return products.slice().reverse();
+// 		}
+// 	};
+
+// 	const { data, error, isLoading, isFetching } = useGetProductsQuery(null);
+// 	const apiProductos = data || [];
+// 	const items = [...apiProductos, ...productos];
+
+// 	if (isLoading || isFetching) return <p>Loading...</p>;
+// 	if (error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>;
+
+// 	const handlePageChange = ({ selected }) => {
+// 		setCurrentPage(selected);
+// 		window.scrollTo(0, 0);
+// 	};
+
+// 	const sortedProducts = sortProducts(items);
+// 	const itemsPerPage = 6;
+// 	const offset = currentPage * itemsPerPage;
+// 	const currentItems = sortedProducts.slice(offset, offset + itemsPerPage);
+// 	const pageCount = Math.ceil(sortedProducts.length / itemsPerPage);
+
+// 	return (
+// 		<>
+// 			<Cards currentItems={currentItems} />
+
+// 			<ReactPaginate
+// 				pageCount={pageCount}
+// 				onPageChange={handlePageChange}
+// 				containerClassName={style['pagination']}
+// 				pageClassName={style['page-item']}
+// 				pageLinkClassName={style['page-link']}
+// 				previousClassName={
+// 					currentPage === 0 ? style['page-item disabled'] : style['page-item']
+// 				}
+// 				previousLinkClassName={style['page-link']}
+// 				nextClassName={
+// 					currentPage === pageCount - 1
+// 						? style['page-item disabled']
+// 						: style['page-item']
+// 				}
+// 				nextLinkClassName={style['page-link']}
+// 				breakClassName={style['page-item']}
+// 				breakLinkClassName={style['page-link']}
+// 				activeClassName={style['active']}
+// 				disabledClassName={style['disabled']}
+// 				forcePage={currentPage}
+// 				marginPagesDisplayed={2}
+// 				pageRangeDisplayed={2}
+// 				previousLabel={currentPage === 0 ? null : 'Back'}
+// 				nextLabel={currentPage === pageCount - 1 ? null : 'Next'}
+// 			/>
+// 		</>
+// 	);
+// };
+
+// export default Paginacion;
