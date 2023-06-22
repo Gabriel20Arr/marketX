ed050be13c08f11e6782020d757f2ccf97fedec8
santiceaglio <santiceagliohf@gmail.com>
2023-06-22 17:22:57 -0300

    cambios en /admin/users

--- client/src/app/admin/users/page.jsx
+++ client/src/app/admin/users/page.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+'use client';
 
-function page() {
-	return <div>Aca vendria el CRUD de usuarios</div>;
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {useGetUsersQuery} from "../../../redux/services/userApi"
 
-export default page;
+const UserList = () => {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	const fetchUsers = async () => {
+		try {
+			const response = await axios.get('http://localhost:3001/Usuario'); // Ruta de la API para obtener la lista de usuarios
+			setUsers(response.data);
+		} catch (error) {
+			console.error('Error fetching users:', error);
+		}
+	};
+
+	const {data, refetch} = useGetUsersQuery(null);
+  useEffect(()=>{
+    refetch();
+  },[])
+  console.log(data);
+
+	// const deleteUser = async (userId) => {
+	// 	try {
+	// 		await axios.delete(`http://localhost:3001/eliminar/${userId}`); // Ruta de la API para eliminar un usuario
+	// 		fetchUsers(); // Actualiza la lista de usuarios después de eliminar uno
+	// 	} catch (error) {
+	// 		console.error('Error deleting user:', error);
+	// 	}
+	// };
+
+	// const deleteUser = async (userId) => {
+	// 	try {
+	// 	  await axios.delete(`http://localhost:3001/eliminar/${userId}`);
+	// 	  fetchUsers();
+	// 	} catch (error) {
+	// 	  console.error('Error deleting user:', error);
+	// 	}
+	//   };
+
+
+
+	  return (
+		<div>
+		  <h1>User List</h1>
+		  <button onClick={() => console.log('Add User')}>Add User</button>
+		  <ul>
+			{data?.map((user) => (
+			  <li key={user._id}> {/* Utilizar user._id en lugar de user.id */}
+				<p>{user.correo}</p>
+				<button onClick={() => console.log('Edit User:', user._id)}>Edit</button>
+				<button onClick={() => console.log('View Details:', user._id)}>View Details</button>
+				<button onClick={() => deleteUser(user._id)}>Delete</button> {/* Utilizar user._id en deleteUser */}
+			  </li>
+			))}
+		  </ul>
+		</div>
+	  );
+};
+
+export default UserList;
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -5,24 +5,28 @@ import Image from 'next/image';
 import logo from '../../images/MarketX-newlogo (2).png';
 import styles from "./NavBar.module.css";
 import { useRouter } from 'next/navigation';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-  
-  export default function Navigation({ currentPath }){
+
+export default function Navigation({ currentPath }) {
   const router = useRouter();
-  const handelrRouter = (value)=>{
+  const handelrRouter = (value) => {
     localStorage.clear();
     router.push(`/${value}`)
   }
 
-  const routerHome = ()=>{
+  const routerDashBoard = () => {
+    router.push('/admin')
+  }
+
+  const routerHome = () => {
     router.push('/home')
   }
 
-  const routerMisProductos = ()=>{
+  const routerMisProductos = () => {
     router.push('/misProductos')
   }
- 
+
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -30,81 +34,89 @@ import React, {useState} from 'react';
   };
 
   const usuarioJSON = localStorage.getItem('usuario');
-    const usuario = JSON.parse(usuarioJSON);
-    console.log(usuario)
+  const usuario = JSON.parse(usuarioJSON);
+  console.log(usuario.rol)
 
-    return (
-      <nav className={styles.container}>
+  return (
+    <nav className={styles.container}>
 
-              <div className={styles.NavConteiner} >
-                <div>
-                    <Image src={logo} className={styles.logo} onClick={routerHome}/>
-                </div>
+      <div className={styles.NavConteiner} >
+        <div>
+          <Image src={logo} className={styles.logo} onClick={routerHome} />
+        </div>
 
-                  {/* <li className="nav-item">
+        {/* <li className="nav-item">
                     <Link className="nav-link" href="/favoritos">Productos favoritos</Link>
                   </li> */}
 
-                 { currentPath !== '/form' && (
-                    <div className={styles.btn}>
-                      <Link style={{ textDecoration: "none", color: "inherit" }} href="/form">Publicar Producto</Link>
-                    </div>
-                  )
-                 }
+        {currentPath !== '/form' && (
+          <div className={styles.btn}>
+            <Link style={{ textDecoration: "none", color: "inherit" }} href="/form">Publicar Producto</Link>
+          </div>
+        )
+        }
 
-                { currentPath !== '/about' && (
-                    <div className={styles.btn}>
-                      <Link style={{ textDecoration: "none", color: "inherit" }} href="/about">Sobre MarketX</Link>
-                    </div>
-                  )
-                 }
+        {currentPath !== '/about' && (
+          <div className={styles.btn}>
+            <Link style={{ textDecoration: "none", color: "inherit" }} href="/about">Sobre MarketX</Link>
+          </div>
+        )
+        }
 
-                  {/* <div className={styles.btn}>
+        {/* <div className={styles.btn}>
                     <Link style={{ textDecoration: "none", color: "inherit" }} href="/loging">Iniciar sesión</Link>
                   </div> */}
 
-                  {/* <div className={styles.btn}>
+        {/* <div className={styles.btn}>
                     <Link style={{ textDecoration: "none", color: "inherit" }} href="/registrarse">Registrarse</Link>
                   </div> */}
-{/* 
+        {/* 
                   <div className={styles.btnExit}>
                     <Link style={{ textDecoration: "none", color: "inherit" }} href="/">Salir</Link>
                   </div> */}
-                  
-                  <div className={styles.dropdown}>
-                    <button className={styles.dropdownToggle} onClick={toggleMenu}>
-                      <h2>
-                        |||
-                      </h2>
-                    </button>
-                  <ul className={`${styles.dropdownMenu} ${isMenuOpen ? styles.show : ""}`}>
-                    <li 
-                    className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }} 
-                    onClick={()=>handelrRouter('loging')}>Iniciar sesión
-                    </li>
-                    <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
-                    onClick={()=>handelrRouter('registrarse')} >Registrarse
-                    </li>
-                    {/* { currentPath !== '/about' && (
+
+        <div className={styles.dropdown}>
+          <button className={styles.dropdownToggle} onClick={toggleMenu}>
+            <h2>
+              |||
+            </h2>
+          </button>
+          <ul className={`${styles.dropdownMenu} ${isMenuOpen ? styles.show : ""}`}>
+            <li
+              className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
+              onClick={() => handelrRouter('loging')}>Iniciar sesión
+            </li>
+            <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
+              onClick={() => handelrRouter('registrarse')} >Registrarse
+            </li>
+            {/* { currentPath !== '/about' && (
                     <li className={styles.dropdownItem}>
                       <Link style={{ textDecoration: "none", color: "inherit" }} href="/about">Sobre MarketX</Link>
                     </li>
                     )} */}
-                    <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
-                    onClick={()=>handelrRouter('')} >Salir
-                    </li>
-                    <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
-                    onClick={routerMisProductos}>mis productos
-                    </li>
-                  </ul>
+            <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
+              onClick={() => handelrRouter('')} >Salir
+            </li>
+            <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
+              onClick={routerMisProductos}>mis productos
+            </li>
+
+            {
+              usuario.rol == 'admin' ?
+                <li className={styles.dropdownItem} style={{ textDecoration: "none", color: "inherit" }}
+                  onClick={routerDashBoard}>Dashboard
+                </li>
+                : null
+            }
+          </ul>
         </div>
 
-                {/* <form className="d-flex" role="search">
+        {/* <form className="d-flex" role="search">
                   <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                   <button className="btn btn-outline-success" type="submit">Search</button>
                 </form> */}
-            
-              </div>
-      </nav>
-    )
+
+      </div>
+    </nav>
+  )
 }
