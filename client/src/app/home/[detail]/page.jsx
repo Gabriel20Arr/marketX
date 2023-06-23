6c9c8adaaf74542c239e1b2910753c2e68f2ca10
ostle <leoostrower@gmail.com>
2023-06-23 01:47:03 -0300

    detail estilos

--- client/src/app/home/[detail]/page.jsx
+++ client/src/app/home/[detail]/page.jsx
@@ -2,77 +2,75 @@
 
 import { useGetProductsByIdQuery } from '@/src/redux/services/productApi';
 import styles from './detail.module.css';
-import Link from "next/link";
+import Link from 'next/link';
 import React, { useContext } from 'react';
-import { Store } from "@/src/utils/Store";
-import { useRouter } from "next/navigation";
-
+import { Store } from '@/src/utils/Store';
+import { useRouter } from 'next/navigation';
 
 export default function Detail({ params }) {
+	const { state, dispatch } = useContext(Store);
 
-  const {state, dispatch} =useContext(Store);
+	const { detail } = params;
+	const { data, error, isLoading, isFetching } = useGetProductsByIdQuery({
+		id: detail,
+	});
+	if (isLoading || isFetching) return <p>Loading...</p>;
+	if (error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>;
 
-  const { detail } = params;
-  const {data, error, isLoading, isFetching} = useGetProductsByIdQuery({ id: detail });
-  if(isLoading || isFetching) return <p>Loading...</p>
-  if(error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>
-
-  const usuarioJSON = localStorage.getItem('usuario');
+	const usuarioJSON = localStorage.getItem('usuario');
 	const usuario = JSON.parse(usuarioJSON);
 
-  const router = useRouter()
+	const router = useRouter();
 
-  const addToCartHandler = () => {
-    const existItem = state.cart.cartItems.find(index => index.id === data.id);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
+	const addToCartHandler = () => {
+		const existItem = state.cart.cartItems.find((item) => item.id === data.id);
+		const quantity = existItem ? existItem.quantity + 1 : 1;
 
-    if(data.stock < quantity) {
-      alert('Lo sentimos, no hay stock de este producto');
-      return;
-    }
+		if (data.stock < quantity) {
+			alert('Lo sentimos, no hay stock de este producto');
+			return;
+		}
 
-    dispatch({type: 'CARD_ADD_ITEM', payload:{ ...data, quantity, usuario:usuario._id }})
-    router.push('/cart');
-  }
+		dispatch({
+			type: 'CART_ADD_ITEM',
+			payload: { ...data, quantity, usuario: usuario._id },
+		});
+		router.push('/cart');
+	};
 
-
-  return (
-    <div className={styles.countForm}>
-
-		<div className={styles.container}>
-			<div className={styles.backLink}>
-				<Link href='/home'>Atrás</Link>
+	return (
+		<>
+			<Link href='/home' className={styles.link}>
+				Volver
+			</Link>
+			<div className={styles.container}>
+				<div className={styles.innerContainer}>
+					<div className={styles.imageContainer}>
+						<img
+							src={data.imagen}
+							alt='error cargando la imagen'
+							className={styles.image}
+						/>
+					</div>
+					<div className={styles.detailsContainer}>
+						<p className={styles.title}>{data.titulo}</p>
+						<p className={styles.price}>${data.precio}</p>
+						<p className={styles.category}>Categoría: {data.categoria}</p>
+						<p className={styles.stock}>Stock: {data.stock}</p>
+						<button
+							className={styles.addButton}
+							disabled={data.stock === 0}
+							onClick={addToCartHandler}
+						>
+							Agregar al carrito
+						</button>
+					</div>
+				</div>
+				<div className={styles.descriptionTitle}>Descripcion</div>
+				<div className={styles.description}>
+					<p>{data.descripcion}</p>
+				</div>
 			</div>
-
-			<div className={styles.image}>
-				<img src={data.imagen} alt='error cargando la imagen' />
-			</div>
-
-      <div className={styles.btn}>
-        <h2 className={styles.price}> Precio: ${data.precio}</h2>
-      </div>
-
-      <div className={styles.btn}>
-        <h2 className={styles.price}> Stock: {data.stock}</h2>
-      </div>
-      
-      <div className={styles.btn}>
-        <h2 className={styles.categoria}>Categoria: {data.categoria}</h2>
-      </div>
-      
-      <div className={styles.btn}>
-        <h2 className={styles.descripcion}>{data.descripcion}</h2>
-      </div>  
-      
-      {/* <div>
-        <h2 className={styles.btn} > cantidadVenta: {data.cantidadVenta}</h2>
-      </div> */}
- 
-
-      <button className="btn btn-primary mt-2" disabled={data.stock === 0} onClick={addToCartHandler}>Agregar al carrito</button>
-
-    </div>
-
-    </div>
-  );
+		</>
+	);
 }
