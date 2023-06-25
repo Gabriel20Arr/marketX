61bcd43ee10d9448b37e1b5c54fc2402b3906541
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-06-25 00:21:13 -0300

    loading

--- client/src/app/admin/page.jsx
+++ client/src/app/admin/page.jsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import styles from './Admin.module.css';
-import Header from '../../components/AdminDashboard/Header/Header';
-import TopCards from '../../components/AdminDashboard/TopCards/TopCards';
-import ChartBar from '../../components/AdminDashboard/ChartBar/ChartBar';
-import RecentOrders from '../../components/AdminDashboard/RecentOrders/RecentOrders';
-import SideBar from '../../components/AdminDashboard/SideBar/SideBar';
+import React from "react";
+import styles from "./Admin.module.css";
+import Header from "../../components/AdminDashboard/Header/Header";
+import TopCards from "../../components/AdminDashboard/TopCards/TopCards";
+import ChartBar from "../../components/AdminDashboard/ChartBar/ChartBar";
+import RecentOrders from "../../components/AdminDashboard/RecentOrders/RecentOrders";
+import SideBar from "../../components/AdminDashboard/SideBar/SideBar";
+import { useSession } from "next-auth/react";
+import Loading from "../../components/Loaders/Loaders";
 
 const Admin = () => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.sideBar}>
-				<SideBar />
-			</div>
-			<div className={styles.content}>
-				<div className={styles.header}>
-					<Header />
-				</div>
-				<div className={styles.main}>
-					<div className={styles.topCharts}>
-						<TopCards />
-					</div>
-					<div className={styles.chartAndOrders}>
-						<div className={styles.chartBar}>
-							<ChartBar />
-						</div>
-						<div className={styles.recentOrders}>
-							<RecentOrders />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const { state } = useSession();
+  if (state === "loading") {
+    return <Loading />;
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.sideBar}>
+        <SideBar />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <Header />
+        </div>
+        <div className={styles.main}>
+          <div className={styles.topCharts}>
+            <TopCards />
+          </div>
+          <div className={styles.chartAndOrders}>
+            <div className={styles.chartBar}>
+              <ChartBar />
+            </div>
+            <div className={styles.recentOrders}>
+              <RecentOrders />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
--- client/src/app/home/[detail]/page.jsx
+++ client/src/app/home/[detail]/page.jsx
@@ -1,93 +1,96 @@
-'use client';
+"use client";
 
-import { useGetProductsByIdQuery } from '@/src/redux/services/productApi';
-import styles from './detail.module.css';
+import { useGetProductsByIdQuery } from "@/src/redux/services/productApi";
+import styles from "./detail.module.css";
 import Link from "next/link";
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { Store } from "@/src/utils/Store";
 import { useRouter } from "next/navigation";
-
+import Loading from "../../../components/Loaders/Loaders";
 
 export default function Detail({ params }) {
-  const {state, dispatch} =useContext(Store);
+  const { state, dispatch } = useContext(Store);
 
   const { detail } = params;
-  const {data, error, isLoading, isFetching} = useGetProductsByIdQuery({ id: detail });
-  if(isLoading || isFetching) return <p>Loading...</p>
-  if(error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>
-  
-  console.log(state, 'data', data);
-  const usuarioJSON = localStorage.getItem('usuario');
-	const usuario = JSON.parse(usuarioJSON);
+  const { data, error, isLoading, isFetching } = useGetProductsByIdQuery({
+    id: detail,
+  });
+  if (isLoading || isFetching) return <p>Loading...</p>;
+  if (error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>;
 
-  const router = useRouter()
+  console.log(state, "data", data);
+  const usuarioJSON = localStorage.getItem("usuario");
+  const usuario = JSON.parse(usuarioJSON);
+
+  const router = useRouter();
 
   const addToCartHandler = () => {
-    const existItem = state.cart.cartItems.find(index => index._id === data._id);
+    const existItem = state.cart.cartItems.find(
+      (index) => index._id === data._id
+    );
     const quantity = existItem ? existItem.quantity + 1 : 1;
 
-    if(data.stock < quantity) {
-      alert('Lo sentimos, no hay stock de este producto');
+    if (data.stock < quantity) {
+      alert("Lo sentimos, no hay stock de este producto");
       return;
     }
 
-    dispatch({type: 'CARD_ADD_ITEM', payload:{ ...data, quantity, usuario:usuario._id }})
+    dispatch({
+      type: "CARD_ADD_ITEM",
+      payload: { ...data, quantity, usuario: usuario._id },
+    });
     // router.push('/cart');
-  }
+  };
 
-	if (isLoading || isFetching) return <p>Loading...</p>;
-	if (error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>;
+  if (isLoading || isFetching) return <Loading />;
+  if (error) return <p>Ha habido un error, vuelve a intentarlo más tarde</p>;
 
   return (
-          <>
-          <div className={styles.link}>
-            <Link href='/home' className={styles.link2}>
-              Volver
-            </Link>
+    <>
+      <div className={styles.link}>
+        <Link href="/home" className={styles.link2}>
+          Volver
+        </Link>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.innerContainer}>
+          <div className={styles.imageContainer}>
+            <img
+              src={data.imagen}
+              alt="error cargando la imagen"
+              className={styles.image}
+            />
           </div>
 
-            <div className={styles.container}>
-              <div className={styles.innerContainer}>
-                <div className={styles.imageContainer}>
-                  <img
-                    src={data.imagen}
-                    alt='error cargando la imagen'
-                    className={styles.image}
-                  />
-                </div>
+          <div className={styles.detailsContainer}>
+            <p className={styles.title}>{data.titulo}</p>
+            <p className={styles.price}>${data.precio}</p>
+            <p className={styles.stock}>Stock: {data.stock}</p>
+            <p className={styles.category}>
+              <div className={styles.categoryC}>Categoría:</div>
+              {data.categoria}
+            </p>
 
-                <div className={styles.detailsContainer}>
-                  <p className={styles.title}>{data.titulo}</p>
-                  <p className={styles.price}>${data.precio}</p>
-                  <p className={styles.stock}>Stock: {data.stock}</p>
-                  <p className={styles.category}>
-                    <div className={styles.categoryC}>
-                      Categoría: 
-                    </div>
-                    {data.categoria}
-                  </p>
-
-                  <div className={styles.descriptionTitle}>
-                    Descripcion
-                    <div>
-                      <p className={styles.description}>{data.descripcion}</p>
-                    </div>
-                  </div>
-
-                  <div className={styles.contenedorCart}>
-                    <button
-                      className={styles.addButton}
-                      disabled={data.stock === 0}
-                      onClick={addToCartHandler}
-                    >
-                      Agregar al carrito
-                    </button>
-                  </div>
-                  
-                </div>
+            <div className={styles.descriptionTitle}>
+              Descripcion
+              <div>
+                <p className={styles.description}>{data.descripcion}</p>
               </div>
-
             </div>
-          </>
+
+            <div className={styles.contenedorCart}>
+              <button
+                className={styles.addButton}
+                disabled={data.stock === 0}
+                onClick={addToCartHandler}
+              >
+                Agregar al carrito
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
