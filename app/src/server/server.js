10474e9a93e86a46143e4f25fc6073aca0487dbd
Gabriel <gabriel2021arr@gmail.com>
2023-06-26 04:01:13 -0300

    show7

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://marketx-production.up.railway.app/');
+  res.header('Access-Control-Allow-Origin', 'https://marketx-doploy.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
