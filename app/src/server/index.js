6d0bb7e1f0aa7bfd446ae9c8e57344668b49b528
Gabriel <gabriel2021arr@gmail.com>
2023-06-26 02:59:54 -0300

    chau2

--- app/src/server/index.js
+++ app/src/server/index.js
@@ -1,7 +1,10 @@
 const app = require('./server');
 const db = require('../database/index');
+require('dotenv').config()
 
-app.listen(3001, () => {
+const { PORT } = process.env
+
+app.listen(PORT, () => {
 	console.log('Servidor Corriendo');
 	db();
 });
