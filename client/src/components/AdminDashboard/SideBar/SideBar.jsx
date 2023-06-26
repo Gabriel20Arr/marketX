368ab639232c2e05dd7bd8a0dab610b29e39377e
Gabriel <gabriel2021arr@gmail.com>
2023-06-26 00:54:48 -0300

    sprint 2 finalizado

--- client/src/components/AdminDashboard/SideBar/SideBar.jsx
+++ client/src/components/AdminDashboard/SideBar/SideBar.jsx
@@ -1,50 +1,4 @@
-// import React from 'react';
-// import Link from 'next/link';
-// import Image from 'next/image';
-// import { RxSketchLogo, RxDashboard, RxPerson } from 'react-icons/rx';
-// import { HiOutlineShoppingBag } from 'react-icons/hi';
-// import { FiSettings } from 'react-icons/fi';
-// import { FaBoxOpen } from 'react-icons/fa';
-
-// import styles from './Sidebar.module.css';
-
-// const Sidebar = ({ children }) => {
-// 	return (
-// 		<div className={styles.container}>
-// 			<div className={styles.sidebar}>
-// 				<Link href='/home'>
-// 					<div className={`${styles.navItem} ${styles.inlineBlock}`}>
-// 						<RxDashboard size={20} />
-// 					</div>
-// 				</Link>
-// 				<Link href='/users'>
-// 					<div className={`${styles.navItem} ${styles.inlineBlock}`}>
-// 						<RxPerson size={20} />
-// 					</div>
-// 				</Link>
-// 				<Link href='/orders'>
-// 					<div className={`${styles.navItem} ${styles.inlineBlock}`}>
-// 						<HiOutlineShoppingBag size={20} />
-// 					</div>
-// 				</Link>
-// 				<Link href='/products'>
-// 					<div className={`${styles.navItem} ${styles.inlineBlock}`}>
-// 						<FaBoxOpen size={20} />
-// 					</div>
-// 				</Link>
-// 				<Link href='/'>
-// 					<div className={`${styles.navItem} ${styles.inlineBlock}`}>
-// 						<FiSettings size={20} />
-// 					</div>
-// 				</Link>
-// 			</div>
-// 			<main className={styles.content}>{children}</main>
-// 		</div>
-// 	);
-// };
-
-// export default Sidebar;
-
+'use client'
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -53,7 +7,7 @@ import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { FiSettings } from 'react-icons/fi';
 import { FaBoxOpen } from 'react-icons/fa';
 
-import styles from './Sidebar.module.css';
+import styles from './SideBar.module.css';
 
 const Sidebar = ({ children }) => {
 	return (
