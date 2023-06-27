630aa5ea8d6ee95799151152f66400cc14ad31f4
Gabriel <gabriel2021arr@gmail.com>
2023-06-27 11:21:08 -0300

    actulizacion de app / back

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -13,20 +13,12 @@ const errorHandler = (err, req, res, next) => {
   res.status(500).json({ error: 'Error interno del servidor' });
 };
 
+// Middlewares generales
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(morgan('dev'));
-app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', 'https://marketx-doploy.vercel.app');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 app.use(express.json());
 app.use(
   fileUpload({
@@ -34,7 +26,20 @@ app.use(
     tempFileDir: "./tmp/",
   })
 );
+
+// Configuración de CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir acceso desde cualquier dominio (Cambia el asterisco por el dominio específico si es necesario)
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+// Rutas
 app.use(require("../routes/index.routes"));
+
+// Manejador de errores
 app.use(errorHandler);
 
 module.exports = app;
