769927e8e50a84e4d33895ccb2b2aafc16d05171
Gabriel <gabriel2021arr@gmail.com>
2023-06-27 11:16:21 -0300

    back actualizado

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'https://marketx-doploy.vercel.app');
-  res.header('Access-Control-Allow-Origin', 'https://client-ten-sandy.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
