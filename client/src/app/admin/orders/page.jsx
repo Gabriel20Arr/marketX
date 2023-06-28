cd37928dbc476e75ee0f1ee56affdec579c464cf
ostle <leoostrower@gmail.com>
2023-06-28 11:03:31 -0300

    panel admin funcional

--- client/src/app/admin/orders/page.jsx
+++ client/src/app/admin/orders/page.jsx
@@ -1,31 +1,29 @@
-'use client'
+'use client';
 
 import React, { useEffect } from 'react';
-import styles from "./orders.module.css"
-import {useGetVentasQuery} from "../../../redux/services/ventasApi"
+import styles from './orders.module.css';
+import { useGetVentasQuery } from '../../../redux/services/ventasApi';
 import Link from 'next/link';
 
 function Page() {
 	const { data, refetch } = useGetVentasQuery(null);
-	
+
 	const ventas = data?.result;
-	
+
 	useEffect(() => {
 		refetch();
 	}, [refetch]);
 
-	
 	// console.log("data: ", ventas);
-	  return (
-    <div>
-      <Link href='/admin' className={styles.link}>
+	return (
+		<div>
+			<Link href='/admin' className={styles.link}>
 				Volver
 			</Link>
-      <h1>Lista de Ventas</h1>
-      <div className={styles.addButtonContainer}>
-      </div>
-      <table className={styles.userTable}>
-        <thead>
+			<h1>Lista de Ventas</h1>
+			<div className={styles.addButtonContainer}></div>
+			<table className={styles.userTable}>
+				<thead>
 					<tr>
 						<th>EMAIL VENDEDOR</th>
 						<th>EMAIL COMPRADOR</th>
@@ -33,18 +31,17 @@ function Page() {
 						<th>FECHA</th>
 					</tr>
 				</thead>
-        {ventas?.map((venta, index) => (
-          <tr key={index}>
-            <td>{venta.vendedor.correo}</td>
-            <td>{venta.comprador.correo}</td>
-            <td>{venta.monto}</td>
-            <td>{venta.fecha}</td>
-          </tr>
-        ))}
-      </table>
-      
-    </div>
-  );
+				{ventas?.map((venta, index) => (
+					<tr key={index}>
+						<td>{venta.vendedor.correo}</td>
+						<td>{venta.comprador.correo}</td>
+						<td>{venta.monto}</td>
+						<td>{new Date(venta.fecha).toLocaleDateString('es-ES')}</td>
+					</tr>
+				))}
+			</table>
+		</div>
+	);
 }
 
 export default Page;
