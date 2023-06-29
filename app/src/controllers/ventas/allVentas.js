b2ab107593bf0533a785c58f8155a6b0e1f36fb6
Gabriel <gabriel2021arr@gmail.com>
2023-06-29 17:54:45 -0300

    actualizacion de modelo ventas

--- app/src/controllers/ventas/allVentas.js
+++ app/src/controllers/ventas/allVentas.js
@@ -9,4 +9,18 @@ const allVentas  = async () => {
   }
 };
 
-module.exports = allVentas;
+const ventasActualizado = async (_id, datosModificados) => {
+  if (!_id ) throw new Error("Se necesita el Id para poder actualizar la venta");
+  if (id.length < 24) throw new Error("El id proporcionado no existe");
+
+  const resultado = await Ventas.findByIdAndUpdate(
+    _id,{ $set: datosModificados },
+    { new: true } // Esto devuelve el documento actualizado en lugar del antiguo
+  );
+
+  if (!resultado) throw new Error("La venta no pudo ser actualizado");
+
+  return resultado;
+};
+
+module.exports = {allVentas, ventasActualizado};
--- app/src/routes/Ventas.router.js
+++ app/src/routes/Ventas.router.js
@@ -1,9 +1,10 @@
 const {Router} = require("express");
 
-const {HandlerAllVentas} = require("../handlers/handler.ventas");
+const {HandlerAllVentas, HandlerActualizar} = require("../handlers/handler.ventas");
 
 const VentasRouter = Router();
 
 VentasRouter.get("/", HandlerAllVentas);
+VentasRouter.put("/actualizar", HandlerActualizar);
 
 module.exports = VentasRouter;
